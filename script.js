a6bd610115ca280deb51106328405439d6c7e398
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-11-07 15:22:34 -0800

    Pseudo code

---
+++ script.js
@@ -0,0 +1,24 @@
+// REVIEW CAROSEL
+
+
+
+// array - containing other people's infromation
+
+
+// select all html elements
+
+
+// set starting item
+
+
+// Load the first person
+
+
+// show person based on item present
+
+
+// Show next person
+
+// Show previous person
+
+// Show a random person
