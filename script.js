7d7b4a95a73d0db4538432f8edbcfdc610d2c30c
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-11-07 15:28:15 -0800

    Grabbed all html elements

--- script.js
+++ script.js
@@ -6,10 +6,16 @@
 
 
 // select all html elements
-
+const img = document.querySelector('.image-container');              //persona's image
+const name = document.querySelector('.name');                        //persona's name
+const title = document.querySelector('.title');                      //persona's title
+const description = document.querySelector('.description');         //persona's title description
+const prevBtn = document.querySelector('.prev-btn');
+const nxtBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
 // set starting item
-
+let currentItem = 0;
 
 // Load the first person
 
@@ -19,6 +25,9 @@
 
 // Show next person
 
+
 // Show previous person
 
+
 // Show a random person
+
