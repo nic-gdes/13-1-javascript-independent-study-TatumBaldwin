76bd7b4520b1b65f8e295d82c539223ad24185f8
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-11-07 15:41:30 -0800

    Got an array with all my personas

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 // REVIEW CAROSEL
-
+const persona = [
+    {
+        id: 1,
+        name: "Lucy Herring",
+        title: "UX DESIGNER",
+        img: "./images/photo-1438761681033-6461ffad8d80.avif",
+        description: "It all started with the computer. Had he known what was to follow, he would have never logged on that day. But the truth was there was no way to know what was about to happen. So Dave pressed the start button, the computer booted up, the screen came alive, and everything Dave knew to be true no longer was."
+    },
+    {
+        id: 2,
+        name: "Emily Damnschen",
+        title: "WEB DESIGNER",
+        img: "./images/photo-1494790108377-be9c29b29330.avif",
+        description: "There are different types of secrets. She had held onto plenty of them during her life, but this one was different. She found herself holding onto the worst type. It was the type of secret that could gnaw away at your insides if you didn't tell someone about it, but it could end up getting you killed if you did."
+    },
+    {
+        id: 3,
+        name: "Mitch Perrywell",
+        title: "ILLUSTRATOR",
+        img: "./images/photo-1506794778202-cad84cf45f1d.avif",
+        description: "The young man wanted a role model. He looked long and hard in his youth, but that role model never materialized. His only choice was to embrace all the people in his life he didn't want to be like."
+    },
+    {
+        id: 4,
+        name: "Ash Mahaffy",
+        title: "VIDEOGRAPHER",
+        img: "./images/photo-1507003211169-0a1dd7228f2d.avif",
+        description: "Welcome to my world. You will be greeted by the unexpected here and your mind will be challenged and expanded in ways that you never thought possible. That is if you are able to survive..."
+    },
+]
 
 
 // array - containing other people's infromation
